dcc0299be427587a5500a7f66bf64ece57b33600
Rafael Souza Nascimento <83884395+RafaSouz@users.noreply.github.com>
2022-09-16 17:35:10 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,29 @@
+from concurrent import futures
+import logging
+import const
+import grpc
+import server_pb2
+import server_pb2_grpc
+
+score = 0
+
+class ScoreData(server_pb2_grpc.ScoreDataServicer):
+
+    def ConsultScore (self):
+        return server_pb2.ValueScore(score) 
+
+    def UpdateScore (self): 
+        score = score + 1
+        return server_pb2.ValueScore(score) 
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server_pb2_grpc.add_ScoreDataServicer_to_server(ScoreData(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
