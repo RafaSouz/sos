dac88bb2b99ff0b8a9b2890018d7ba4a5b031311
Rafael Souza Nascimento <83884395+RafaSouz@users.noreply.github.com>
2022-09-16 17:34:36 -0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import logging
+from urllib import response
+import grpc
+import server_pb2
+import server_pb2_grpc
+import sys
+import const
+
+def ShowScore():
+    with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
+        stub = server_pb2_grpc.ScoreDataStub(channel)
+        response = stub.ConsultScore(server_pb2.EmptyMessage())
+        print(f'Score Atual: {response}\n')
+
+def UpdateScore():
+    with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
+        stub = server_pb2_grpc.ScoreDataStub(channel)
+        new = stub.UpdateScore(server_pb2.EmptyMessage())
+        print(f'Novo Score: {new}\n')
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    while True:
+        choice = int(input("Digite 1 para ver o Score e 2 para aumentar o Score: "))
+        if(choice == 1):
+            ShowScore()
+        else:   
+            UpdateScore()
